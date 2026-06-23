07d097aace7b41a765e53f026acb93e556b55048
Pedrords41 <41981@raphaeldisanto.com.br>
2026-06-23 08:43:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Animação ao rolar a página
+
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+sections.forEach(section => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
